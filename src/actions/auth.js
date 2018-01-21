1875d33cd471588836a35c24bfee03b03bc714a5
Dinesh-Balaji-Saravanan <androbalajiupdated@gmail.com>
2018-01-21 16:26:19 +0530

    Delete all the stuff

--- src/actions/auth.js
+++
@@ -1,9 +0,0 @@
-import {USER_LOGGED_IN} from "../types";
-import api from '../api';
-
-export const userLoggedIn = user => ({
-   type: USER_LOGGED_IN,
-    user
-});
-
-export const login = (creadentials) => dispatch => api.user.login(creadentials).then(user => dispatch(userLoggedIn(user)));
