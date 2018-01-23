90e45901b7e1bc161c97c50747bf271725551f3f
Dinesh-Balaji-Saravanan <androbalajiupdated@gmail.com>
2018-01-23 08:47:31 +0530

    make it better again 6

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -14,7 +14,7 @@ class Dashboard extends React.Component {
             <div id={"padding1"} className={"container "}>
                 <div className={"row card border border-top-0 border-bottom-0 border-light bg-light" }>
                     <div id={"padding-all"} className={"col-12"}>
-                        <h4 className={"card-body display-4"} align="center"> <img src={require("../images/dash.png")} height={75}/> Dashboard
+                        <h4 className={"card-body display-4"} align="center"> <img src={require("../images/dash.png")} height={75} alt={"Dash"}/> Dashboard
                             <hr/></h4>
                         <Route exact path={"/dashboard"} component={DashCont}  />
                         <Route path={"/dashboard/add_site"} component={AddSite}  />
--- src/footer/footer.js
+++ src/footer/footer.js
@@ -10,11 +10,11 @@ class Footer extends React.Component {
                 <br/>
                 <p className="mb-1">&copy; 2017-2018 Mudita Development</p>
                 <ul className="list-inline">
-                    <li className="list-inline-item"><a className={"card-link text-dark"} href="#">Login</a></li>
-                    <li className="list-inline-item"><a className={"card-link text-dark"} href="#">Sign Up</a></li>
-                    <li className="list-inline-item"><a className={"card-link text-dark"} href="#">Settings</a></li>
+                    <li className="list-inline-item"><a className={"card-link text-dark"} href="../signin">Login</a></li>
+                    <li className="list-inline-item"><a className={"card-link text-dark"} href="../signin">Sign Up</a></li>
+                    <li className="list-inline-item"><a className={"card-link text-dark"} href="../signin">Settings</a></li>
                 </ul>
-                <p className="mb-1"><a className={"text-dark card-link"} href={"https://github.com/Dinesh-Balaji-Saravanan/react-signup-app"}><img src={require("../images/git.png")} height={35} /> GitHub</a></p>
+                <p className="mb-1"><a className={"text-dark card-link"} href={"https://github.com/Dinesh-Balaji-Saravanan/react-signup-app"}><img src={require("../images/git.png")} height={35} alt={"git"} /> GitHub</a></p>
             </footer>
         );
     }
